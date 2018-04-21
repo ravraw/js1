72753705a3367302533ed35a0cf0e7b5e6639794
ravindra rawat <ravinrawat.wd@gmail.com>
2018-04-21 18:46:34 +0000

    3 player form

--- main.js
+++ main.js
@@ -1,49 +1,63 @@
-console.log("HELLO WORLD");
+const ageP1 = document.getElementById("p1__age");
+const heightP1 = document.getElementById("p1__height");
+const ageP2 = document.getElementById("p2__age");
+const heightP2 = document.getElementById("p2__height");
+const ageP3 = document.getElementById("p3__age");
+const heightP3 = document.getElementById("p3__height");
 
-var JohnAge = prompt("Pls. enter Johns age");
-var JohnHieght = prompt("Pls. enter Johns height in CMS");
-var friendAge = prompt("Pls. enter FRIENDS age");
-var friendHieght = prompt("Pls. enter FRIENDS height in CMS");
-var Player3Age = prompt("Pls. enter Player 3 age");
-var Player3Height = prompt("Pls. enter Player 3 height in CMS");
+let winner = "Click button for winner";
 
-function score(age, height) {
+// check winner button
+document.getElementById("button").addEventListener("click", () => {
+  checkWinner();
+  document.getElementById("winner").innerHTML = winner;
+});
+
+// reset button
+document.getElementById("reset").addEventListener("click", () => {
+  document.getElementById("winner").innerHTML = "Click button for   winner";
+  let para = document.querySelectorAll("p + p");
+  for (i in para) {
+    para[i].innerHTML = "";
+  }
+  let input = document.getElementsByTagName("input");
+  for (i in input) {
+    input[i].value = "";
+  }
+});
+
+function score(height, age) {
   return age * 5 + height;
 }
 
-var johnScore = score(+JohnAge, +JohnHieght);
-var friendScore = score(+friendAge, +friendHieght);
-var Player3Score = score(+Player3Age, +Player3Height);
+function checkWinner() {
+  let scoreP1 = score(+heightP1.value, +ageP1.value); // + converts strings to number
+  let scoreP2 = score(+heightP2.value, +ageP2.value);
+  let scoreP3 = score(+heightP3.value, +ageP3.value);
 
-var scores =
-  "Johns's score :  " +
-  johnScore +
-  " " +
-  "Friend's score :" +
-  " " +
-  friendScore +
-  "PLayer3's score :" +
-  " " +
-  Player3Score;
+  document.getElementById("score1").innerHTML = "Score : " + " " + scoreP1;
+  document.getElementById("score2").innerHTML = "Score : " + " " + scoreP2;
+  document.getElementById("score3").innerHTML = "Score : " + " " + scoreP3;
 
-if ((johnScore === friendScore) === Player3Score) {
-  alert("Its a tie" + scores);
-} else {
-  if (johnScore > friendScore) {
-    if (johnScore > Player3Score) {
-      alert("John wins !!!" + scores);
-    } else if (johnScore === Player3Score) {
-      alert("Its a tie" + scores);
+  if (scoreP1 > scoreP2) {
+    if (scoreP1 === scoreP3) {
+      winner = "p1 & p3 winner";
+    } else if (scoreP1 > scoreP3) {
+      winner = "p1 winner";
+    }
+  } else if (scoreP1 === scoreP2) {
+    if (scoreP2 === scoreP3) {
+      winner = "p1 ,p2, p3 are winner";
+    } else if (scoreP2 > scoreP3) {
+      winner = "p1 & p2 are winner";
+    }
+  } else if (scoreP2 >= scoreP3) {
+    if (scoreP2 > scoreP3) {
+      winner = "p2 winner";
     } else {
-      alert("Player 3 wins !!!" + scores);
+      winner = "p2 & p3 winner";
     }
   } else {
-    if (friendScore > Player3Score) {
-      alert("Friend wins !!!" + scores);
-    } else if (friendScore === Player3Score) {
-      alert("Its a tie" + scores);
-    } else {
-      alert("Player 3 wins !!!" + scores);
-    }
+    winner = "p3 is winner";
   }
 }
